437568864c00cbea39496edce07f6ae0e7af41ed
Pavel-Baturin <pavel.baturin.92@gmail.com>
2021-11-17 20:33:24 +0200

    Добавляет решение задачи №1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+import SimpleLightbox  from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryImages = document.querySelector('.gallery');
+const murkupGallery = createGalleryImages(galleryItems);
+galleryImages.insertAdjacentHTML('beforeend', murkupGallery);
+galleryImages.addEventListener('click', onImageClick)
+
+
+function createGalleryImages(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+    `;
+    }).join("");
+}
+
+function onImageClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    
+   var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt',captionDelay: 250,});
+}
+
