ca41a35626c3f632f2a712d1c84a25a199b2d2d4
Pavel-Baturin <pavel.baturin.92@gmail.com>
2021-11-18 09:27:45 +0200

    Добавляет решение задачи №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const onPlayer = function (data) {
+    const currentTime = Number(data.seconds);
+    localStorage.setItem("videoplayer-current-time", currentTime);
+};
+
+player.on('timeupdate', throttle(onPlayer, 1000));
+
+
+const savedCurrentTime = localStorage.getItem("videoplayer-current-time");
+  player.setCurrentTime(savedCurrentTime).then(function(seconds) {
+   
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            
+            break;
+
+        default:
+            
+            break;
+    }
+});
